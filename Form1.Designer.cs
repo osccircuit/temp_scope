edc979a450a77ffb882b4bd88c251deb22121b2c
Egor Popov <popoveg18@gmail.com>
2023-11-19 18:42:40 +0300

    Дизайн
    Расставлены все основные элементы, которые будут использоваться в
    программе

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,6 +2,7 @@
 
 partial class Form1
 {
+    
     /// <summary>
     ///  Required designer variable.
     /// </summary>
@@ -30,15 +31,109 @@ partial class Form1
     {
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        this.ClientSize = new System.Drawing.Size(900, 450);
+        this.Text = "TermoScope";
 
+        //Лейблы
+        Label label1 = new Label();
+        label1.Text = "Запуск измерения:";
+        label1.Size = new Size(120, 16);
+        label1.Location = new Point(10, 25);
+        this.Controls.Add(label1);
+
+        Label label2 = new Label();
+        label2.Text = "Остановить измерение:";
+        label2.Size = new Size(140, 16);
+        label2.Location = new Point(140, 25);
+        this.Controls.Add(label2);
+
+        Label label3 = new Label();
+        label3.Text = "Построение карты:";
+        label3.Size = new Size(140, 16);
+        label3.Location = new Point(10, 70);
+        this.Controls.Add(label3);
+
+        //Разделители
+        Label separatorLabel1 = new Label();
+        separatorLabel1.BackColor = System.Drawing.SystemColors.ControlDark;
+        separatorLabel1.Size = new Size(280, 2);
+        separatorLabel1.Location = new Point(5, 115); // Установите положение линии на форме
+        this.Controls.Add(separatorLabel1);
+
+        Panel verticalLine = new Panel();
+        verticalLine.Width = 2; // Ширина вертикальной линии
+        verticalLine.BackColor = System.Drawing.Color.Gray; // Цвет фона линии
+        verticalLine.Location = new Point(288, 25); // Установите положение линии на форме
+        verticalLine.Height = 400; // Высота вертикальной линии
+        this.Controls.Add(verticalLine);
+
+        //Кнопки
         Button button1 = new Button();
-        button1.Text = "Нажми меня"; // Установите текст кнопки
-        button1.Location = new Point((this.ClientSize.Width - button1.Width) / 2, (this.ClientSize.Height - button1.Height) / 2); // Разместите кнопку в середине экрана
-        //button1.Location = new System.Drawing.Point(0, 0); // Установите положение кнопки на форме (координаты X и Y)
+        button1.Text = "Старт"; // Установите текст кнопки
+        //button1.Location = new Point((this.ClientSize.Width - button1.Width) / 2, (this.ClientSize.Height - button1.Height) / 2); // Разместите кнопку в середине экрана
+        button1.Location = new System.Drawing.Point(10, 40); // Установите положение кнопки на форме (координаты X и Y)
         button1.Click += new EventHandler(button1_Click); // Добавьте обработчик события нажатия на кнопку
         this.Controls.Add(button1); // Добавьте кнопку на форму
+
+        Button button2 = new Button();
+        button2.Text = "Стоп"; // Установите текст кнопки
+        //button1.Location = new Point((this.ClientSize.Width - button1.Width) / 2, (this.ClientSize.Height - button1.Height) / 2); // Разместите кнопку в середине экрана
+        button2.Location = new System.Drawing.Point(142, 40); // Установите положение кнопки на форме (координаты X и Y)
+        //button1.Click += new EventHandler(button2_Click); // Добавьте обработчик события нажатия на кнопку
+        this.Controls.Add(button2); // Добавьте кнопку на форму
+        //Панель элементов
+        ToolStrip toolStrip1 = new ToolStrip();
+        toolStrip1.Dock = DockStyle.Top; // Устанавливает докирование панели инструментов в верхней части формы.
+        // Создайте кнопки или другие элементы управления для панели инструментов.
+        ToolStripDropDownButton dropDownButton = new ToolStripDropDownButton("Файл");
+
+        ToolStripMenuItem menuItem1 = new ToolStripMenuItem("Найти и подключиться");
+        ToolStripMenuItem menuItem2 = new ToolStripMenuItem("Отключить устройство");
+        ToolStripMenuItem menuItem3 = new ToolStripMenuItem("Запуск измерения");
+        ToolStripMenuItem menuItem4 = new ToolStripMenuItem("Остановить измерение");
+        ToolStripMenuItem menuItem5 = new ToolStripMenuItem("Сохранить полученную карту");
+        ToolStripMenuItem menuItem6 = new ToolStripMenuItem("Выход");
+
+        // Добавьте элементы в выпадающее меню
+        dropDownButton.DropDownItems.Add(menuItem1);
+        dropDownButton.DropDownItems.Add(menuItem2);
+        dropDownButton.DropDownItems.Add(menuItem3);
+        dropDownButton.DropDownItems.Add(menuItem4);
+        dropDownButton.DropDownItems.Add(menuItem5);
+        dropDownButton.DropDownItems.Add(menuItem6);
+
+        
+        ToolStripButton button_strip_2 = new ToolStripButton("Сохранить карту");
+        ToolStripButton button_strip_3 = new ToolStripButton("Выход");
+
+        // Добавьте элементы управления на панель инструментов.
+        toolStrip1.Items.Add(dropDownButton);
+        toolStrip1.Items.Add(button_strip_2);
+        toolStrip1.Items.Add(button_strip_3);
+
+        // Добавьте обработчики событий для кнопок (если необходимо).
+        //button1.Click += Button1_Click;
+        //button2.Click += Button2_Click;
+        //button3.Click += Button3_Click;
+
+        // Добавьте панель инструментов на форму.
+        this.Controls.Add(toolStrip1);
+
+        //Строка состояния
+        StatusStrip statusStrip1 = new StatusStrip();
+        this.Controls.Add(statusStrip1); // Добавьте строку состояния на форму
+        ToolStripStatusLabel toolStripStatusLabel1 = new ToolStripStatusLabel();
+        toolStripStatusLabel1.Text = "Готово"; // Установите текст элемента
+        statusStrip1.Items.Add(toolStripStatusLabel1); // Добавьте элемент на строку состояния
+
+        //Прогрессбар
+        ProgressBar progressBar1 = new ProgressBar();
+        progressBar1.Minimum = 0; // Минимальное значение
+        progressBar1.Maximum = 100; // Максимальное значение
+        progressBar1.Value = 50; // Текущее значение
+        progressBar1.Size  = new Size(260, 16);
+        progressBar1.Location = new Point(12, 86);
+        this.Controls.Add(progressBar1);
     }
 
     #endregion
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,31 @@
 namespace temp_scope;
-
 public partial class Form1 : Form
 {
+    readonly ScottPlot.FormsPlot formsPlot1;
     public Form1()
     {
         InitializeComponent();
+
+        //Add the FormsPlot
+        //formsPlot1 = new() { Dock = DockStyle.Fill };
+        formsPlot1 = new();
+        formsPlot1.Width = 600; // Установите желаемую ширину элемента
+        formsPlot1.Height = 420; // Установите желаемую высоту элемента
+        formsPlot1.Location = new Point(300, 20);
+        Controls.Add(formsPlot1);
+ 
+        // Add sample data to the plot
+        double[,] data = { { 1, 2, 3 }, { 4, 5, 6 } };
+        var hm = formsPlot1.Plot.AddHeatmap(data);
+        var cb = formsPlot1.Plot.AddColorbar(hm);
+        formsPlot1.Plot.Title("Температурная карта");
+        formsPlot1.Refresh();
+        //var plt = new ScottPlot.Plot(200, 200);
+        //double[,] data2D = { { 1, 2, 3 },
+        //{ 4, 5, 6 } };
+
+        //plt.AddHeatmap(data2D);
+        //plt.Refresh();
     }
 
     private void button1_Click(object sender, EventArgs e)
